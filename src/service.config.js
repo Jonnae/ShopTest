4d0317d2d7739f7b327e4566ec548c868595292a
Jonnae <chkimm@163.com>
2020-04-26 17:29:42 +0800

    地址统一管理

--- src/service.config.js
+++ src/service.config.js
@@ -1,9 +1,9 @@
 const MOCKURL = 'http://www.123.com/'; //mock接口
-const SERVEURL = 'http://localhost:3000/'//真实的接口地址
+const SERVEURL = 'http://localhost:3000/' //真实的接口地址
 
 const URL = {
     getVarietyItem: MOCKURL + 'getVarietyItem',
-    registUser: SERVEURL + 'user/registUser',//user对应后端控制器controller,registUser对应user下面的一个方法
+    registUser: SERVEURL + 'user/registUser', //user对应后端控制器controller,registUser对应user下面的一个方法
     logintUser: SERVEURL + 'user/logintUser',
     getTypes: SERVEURL + 'type/getTypes',
     getProductListByType: SERVEURL + 'product/getProductListByType',

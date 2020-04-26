d6cdbd239a40e1efc7603ee31c49d9764e19f8d9
Jonnae <chkimm@163.com>
2020-04-26 17:31:10 +0800

    vant引入

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import {SubmitBar,Card,PullRefresh,List,Row,Col,NavBar,Icon,Tabbar,TabbarItem,Swipe, SwipeItem,Lazyload,Tab,Tabs,Field,CellGroup,Button,GoodsAction, GoodsActionIcon, GoodsActionButton} from 'vant';
+import {SubmitBar,Card,PullRefresh,Swipe, SwipeItem,Lazyload,List,Row,Col,NavBar,Icon,Tabbar,TabbarItem,Tab,Tabs,Field,CellGroup,Button,GoodsAction, GoodsActionIcon, GoodsActionButton} from 'vant';
 
 Vue.use(SubmitBar).use(Card).use(GoodsAction).use(GoodsActionIcon).use(GoodsActionButton).use(PullRefresh).use(List).use(Row).use(Col).use(Field).use(Button).use(CellGroup).use(Tab).use(Tabs).use(NavBar).use(Icon).use(Tabbar).use(TabbarItem).use(Swipe).use(SwipeItem).use(Lazyload)
 

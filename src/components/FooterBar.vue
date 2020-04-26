1fa6ba7d2535805b0598b5d05a2348deba3e6deb
Jonnae <chkimm@163.com>
2020-04-26 17:31:59 +0800

    头部组件

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,33 +1,31 @@
 <template>
-    <div>
+  <div>
     <van-tabbar v-model="active" @change="change">
-        <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
-        <van-tabbar-item icon="label-o" to="/category">分类</van-tabbar-item>
-        <van-tabbar-item icon="cart-o" to="/cart">购物车</van-tabbar-item>
-        <van-tabbar-item icon="user-o" to="/profile">我的</van-tabbar-item>
+      <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+      <van-tabbar-item icon="label-o" to="/category">分类</van-tabbar-item>
+      <van-tabbar-item icon="cart-o" to="/cart">购物车</van-tabbar-item>
+      <van-tabbar-item icon="user-o" to="/profile">我的</van-tabbar-item>
     </van-tabbar>
-    </div>
+  </div>
 </template>
 
 <script>
-
-    export default {
-        data() {
-            return {
-                active: 0
-         }    
-        },
-    created() {
-        this.active = parseInt(localStorage.getItem('active'))
-    },
+export default {
+  data() {
+    return {
+      active: 0
+    };
+  },
+  created() {
+    this.active = parseInt(localStorage.getItem("active"));
+  },
   methods: {
-            change(active){
-             localStorage.setItem('active',active)   
-            },
-        },
+    change(active) {
+      localStorage.setItem("active", active);
     }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
